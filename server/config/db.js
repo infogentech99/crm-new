c220c68e8cef6d4723861a75e8bd170bba4e3057
Info gentech <infogentech99@gmail.com>
2025-07-03 16:21:27 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -8,7 +8,11 @@ export const connectDB = async () => {
     mongoose.set('strictQuery', false);
     mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
-      useUnifiedTopology: true
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 30000, // Keep trying to send operations for 30 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+      tls: true, // Explicitly enable TLS
+      tlsAllowInvalidCertificates: false // Do not allow invalid certificates
     });
     console.log('MongoDB connected');
   } catch (err) {
