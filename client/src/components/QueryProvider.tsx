c945109f234be2cd427cf4e5254f1facbfdddb18
Prakash Chand <Prakash0719pc@gmail.com>
2025-07-05 15:03:27 +0530

    Fixed:Extra call API

--- client/src/components/QueryProvider.tsx
+++ client/src/components/QueryProvider.tsx
@@ -3,7 +3,13 @@
 import React from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
   return (
